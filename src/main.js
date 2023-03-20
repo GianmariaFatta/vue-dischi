3b85fe63b4b2b08b2a1193658248425a8c9348ca
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-20 16:35:34 +0100

    add router component

--- src/main.js
+++ src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import App from './App.vue'
 import { formToJSON } from 'axios'
+import {router} from './components/router/'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
+
+
+
