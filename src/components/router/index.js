a2c2da707f31c38227c89467ef64666869991087
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-20 16:35:58 +0100

    add link active and pages

---
+++ src/components/router/index.js
@@ -0,0 +1,17 @@
+import {createRouter , createWebHistory} from 'vue-router';
+
+import  HomePage from '../pages/HomePage.vue';
+import  NotFoundPage from '../pages/NotFoundPage.vue';
+
+const router =createRouter({
+    history:createWebHistory(),
+    linkActiveClass:'active',
+    linkExactActiveClass:'active',
+    routes:[
+        {path:'/' , name:'home', component:HomePage },
+        {path:'/:pathMatch(.*)*' , name:'not-found', component:NotFoundPage },
+
+    ]
+})
+
+export{router}
