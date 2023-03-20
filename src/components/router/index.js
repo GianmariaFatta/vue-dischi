0333f60660ec52ca1ec5492cea5525efceb62cdc
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-20 16:54:34 +0100

    Added ContactsPage

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,17 +1,18 @@
-import {createRouter , createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
-import  HomePage from '../pages/HomePage.vue';
-import  NotFoundPage from '../pages/NotFoundPage.vue';
+import HomePage from "../pages/HomePage.vue";
+import ContactsPage from "../pages/ContactsPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
-const router =createRouter({
-    history:createWebHistory(),
-    linkActiveClass:'active',
-    linkExactActiveClass:'active',
-    routes:[
-        {path:'/' , name:'home', component:HomePage },
-        {path:'/:pathMatch(.*)*' , name:'not-found', component:NotFoundPage },
+const router = createRouter({
+  history: createWebHistory(),
+  linkActiveClass: "active",
+  linkExactActiveClass: "active",
+  routes: [
+    { path: "/", name: "home", component: HomePage },
+    { path: "/contacts", name: "contact", component: ContactsPage },
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
+  ],
+});
 
-    ]
-})
-
-export{router}
+export { router };
