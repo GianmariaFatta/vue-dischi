664b1c5343143b29f13d67e73c3b2985261bdeee
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-03-20 16:36:22 +0100

    created homepage

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script>
+import axios from 'axios';
+export default {
+    name: 'Homepage',
+};
+</script>
+
+<template>
+    <div class="container text-center my-5">
+        <h1>VIDEOGAMES</h1>
+
+    </div>
+</template>
+
+<style></style>
