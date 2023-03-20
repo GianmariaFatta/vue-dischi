d37c4fa4182f95251d62fdb938edad3b2ec3a76d
AndreaMinelli <minelliandrea94@gmail.com>
2023-03-20 19:12:00 +0100

    Created videogame detail page and route

---
+++ src/components/pages/VideogameDetailPage.vue
@@ -0,0 +1,37 @@
+<script>
+const baseProjectsUrl = "http://127.0.0.1:8000/api";
+import axios from "axios";
+export default {
+  name: "VideogameDetailPage",
+  data() {
+    return {
+      videogame: null,
+    };
+  },
+  methods: {
+    fetchVideogame() {
+      const endpoint = baseProjectsUrl + "/videogame/" + this.$route.params.id;
+      axios
+        .get(endpoint)
+        .then((res) => {
+          this.videogame = res.data;
+        })
+        .catch(() => {
+          this.$router.push({ name: "not-found" });
+        })
+        .then(() => {});
+    },
+  },
+  created() {
+    this.fetchVideogame();
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center">{{ videogame.title }}</h1>
+  </div>
+</template>
+
+<style></style>
--- src/components/router/index.js
+++ src/components/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "../pages/HomePage.vue";
 import ContactsPage from "../pages/ContactsPage.vue";
+import VideogameDetailPage from "../pages/VideogameDetailPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
 const router = createRouter({
@@ -11,6 +12,11 @@ const router = createRouter({
   routes: [
     { path: "/", name: "home", component: HomePage },
     { path: "/contacts", name: "contact", component: ContactsPage },
+    {
+      path: "/videogame/:id",
+      name: "videogame-details",
+      component: VideogameDetailPage,
+    },
     { path: "/:pathMatch(.*)*", name: "not-found", component: NotFoundPage },
   ],
 });
